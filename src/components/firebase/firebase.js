cd0a7a72e79c573418053ae762fe53b29ae76c2b
David Yen <davidyen@Davids-iMac.local>
2018-02-09 12:17:13 -0800

    Refactored Navbar. WIP - fix for  - fix for mobile

---
+++ src/components/firebase/firebase.js
@@ -0,0 +1,36 @@
+import * as firebase from 'firebase';
+
+const env = 'prod';
+// const env = 'dev';
+
+const devConfig = {
+  apiKey: "AIzaSyDT4P0K7vKjikHupDEnNwWOcHISI_Z7s6Y",
+  authDomain: "react-firebase-b84a7.firebaseapp.com",
+  databaseURL: "https://react-firebase-b84a7.firebaseio.com",
+  projectId: "react-firebase-b84a7",
+  storageBucket: "react-firebase-b84a7.appspot.com",
+  messagingSenderId: "565346356211"
+};
+
+const prodConfig = {
+  apiKey: "AIzaSyCfvK9YdCmfSaibrPymBWVoGVeVTRDS8Fw",
+  authDomain: "react-firebase-prod.firebaseapp.com",
+  databaseURL: "https://react-firebase-prod.firebaseio.com",
+  projectId: "react-firebase-prod",
+  storageBucket: "react-firebase-prod.appspot.com",
+  messagingSenderId: "139452593418"
+};
+
+const config = env === 'prod'
+  ? prodConfig
+  : devConfig;
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(config);
+}
+
+const auth = firebase.auth();
+
+export{
+  auth
+};
